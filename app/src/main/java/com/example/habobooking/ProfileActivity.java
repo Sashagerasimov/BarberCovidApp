32218335f79babd8a4a18fbd4b0eb84b0d8eb74e
Collin Zhang <collin.zhang17@gmail.com>
2020-07-31 21:10:56 +1000

    Update Profile Function Working

--- app/src/main/java/com/example/habobooking/ProfileActivity.java
+++ app/src/main/java/com/example/habobooking/ProfileActivity.java
@@ -1,8 +1,11 @@
 package com.example.habobooking;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.sip.SipSession;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -24,15 +27,57 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.ListenerRegistration;
+import com.google.firebase.firestore.SetOptions;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ProfileActivity extends AppCompatActivity {
 
+    private static final String TAG = "";
     private TextInputEditText profileName, profileAddress, profileNumber;
     private Button updateBtn;
-    private FirebaseAuth firebaseAuth;
-    private FirebaseDatabase firebaseDatabase;
+    private static final String KEY_PROFILENAME = "name";
+    private static final String KEY_PROFILENUMBER = "phoneNumber";
+    private static final String KEY_PROFILEADDRESS = "address";
     String DISPLAY_NAME = null;
     CollectionReference userRef;
+
+    private FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    private DocumentReference documentReference = db.collection("User").document(Common.currentUser.getPhoneNumber());
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+                if(e !=  null){
+                    Toast.makeText(ProfileActivity.this, "Error!", Toast.LENGTH_SHORT).show();
+                    Log.d(TAG, "onEvent: "+ e.toString());
+                    return;
+                }
+
+                if (documentSnapshot.exists()){
+                    String name = documentSnapshot.getString(KEY_PROFILENAME);
+                    String address = documentSnapshot.getString(KEY_PROFILEADDRESS);
+                    String number = documentSnapshot.getString(KEY_PROFILENUMBER);
+
+                    profileName.setText(name);
+                    profileAddress.setText(address);
+                    profileNumber.setText(number);
+                }
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,25 +89,16 @@ public class ProfileActivity extends AppCompatActivity {
         profileNumber = findViewById(R.id.etProfilePhoneNumber);
         updateBtn = findViewById(R.id.updateProfileBtn);
 
-        firebaseAuth = FirebaseAuth.getInstance();
-        firebaseDatabase = FirebaseDatabase.getInstance();
-
-        DatabaseReference databaseReference = firebaseDatabase.getReference(firebaseAuth.getUid());
-
-        databaseReference.child("name").setValue("bill");
-
-        profileName.setText(Common.currentUser.getName());
-        profileAddress.setText(Common.currentUser.getAddress());
-        profileNumber.setText(Common.currentUser.getPhoneNumber());
-
         updateBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String name = profileName.getText().toString().trim();
-                String address = profileAddress.getText().toString().trim();
-                String phone = profileNumber.getText().toString().trim();
+                updateUser(view);
 
-                updateUser("name", phone, address);
+               //String name = profileName.getText().toString().trim();
+               //String address = profileAddress.getText().toString().trim();
+               //String phone = profileNumber.getText().toString().trim();
+
+               //updateUser("name", phone, address);
             }
         });
 
@@ -90,17 +126,80 @@ public class ProfileActivity extends AppCompatActivity {
        //});
     }
 
-    private boolean updateUser(String name, String phone, String address){
-        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("User").child(phone);
+    public void updateUser(View v){
+        String name = profileName.getText().toString();
+        String address = profileAddress.getText().toString();
 
-        User user = new User(name, phone, address);
+        Map<String, Object> profile = new HashMap<>();
+        profile.put(KEY_PROFILENAME, name);
+        profile.put(KEY_PROFILEADDRESS, address);
 
-        databaseReference.setValue(user);
-
-        Toast.makeText(this, "User Updated Succesfully", Toast.LENGTH_LONG).show();
-
-        return true;
+        documentReference.update(profile);
     }
+
+    public void saveUser(View v){
+        String name = profileName.getText().toString();
+        String address = profileAddress.getText().toString();
+        String number = profileNumber.getText().toString();
+
+        Map<String, Object> profile = new HashMap<>();
+        profile.put(KEY_PROFILENAME, name);
+        profile.put(KEY_PROFILEADDRESS, address);
+        profile.put(KEY_PROFILENUMBER, number);
+
+        documentReference.set(profile)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(ProfileActivity.this, "Profile saved", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(ProfileActivity.this, "Error!", Toast.LENGTH_SHORT).show();
+                        Log.d(TAG, e.toString());
+                    }
+                });
+    }
+
+    public void loadUser(View v){
+        documentReference.get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        if (documentSnapshot.exists()){
+                            String name = documentSnapshot.getString(KEY_PROFILENAME);
+                            String address = documentSnapshot.getString(KEY_PROFILEADDRESS);
+                            String number = documentSnapshot.getString(KEY_PROFILENUMBER);
+
+                            profileName.setText(name);
+                            profileAddress.setText(address);
+                            profileNumber.setText(number);
+                        } else {
+                            Toast.makeText(ProfileActivity.this, "Document does not exist", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(ProfileActivity.this, "Error!", Toast.LENGTH_SHORT).show();
+                        Log.d(TAG, e.toString());
+                    }
+                });
+    }
+   // private boolean updateUser(String name, String phone, String address){
+   //     DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("User").child(phone);
+//
+   //     User user = new User(name, phone, address);
+//
+   //     databaseReference.setValue(user);
+//
+   //     Toast.makeText(this, "User Updated Succesfully", Toast.LENGTH_LONG).show();
+//
+   //     return true;
+   // }
 
    // public void updateProfile(View view){
    //     DISPLAY_NAME = profileName.getText().toString();
